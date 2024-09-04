b7301f0e6c24ea8fcfab57421638516ab464e4c4
Saekit <raquel.sae.randall@gmail.com>
2024-09-04 14:21:00 -0400

    fix image height

--- src/components/product-image/ProductImage.styles.js
+++ src/components/product-image/ProductImage.styles.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const DefaultImage = styled.img`
   display: block;
   width: 100%;
-  height: auto;
+  height: 100%;
+  object-fit: cover;
   border-top-right-radius: 25px;
   border-top-left-radius: 25px;
   border-bottom-left-radius: 0;
